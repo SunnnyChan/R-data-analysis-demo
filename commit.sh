a873fce5413a66690566f893b8f2db4bc4bcb0aa
sunnychan <sunnnychan@gmail.com>
2020-06-10 14:31:01 +0800

    sunnychan

--- commit.sh
+++ commit.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 github() {
    git config --global user.name sunnychan
    git config --global user.email sunnnychan@gmail.com
@@ -8,5 +7,4 @@ github() {
    git $@
 }
 
-cp /Users/sunny/WorkPlace/jupyter/data/Jupyter-Python-Data-Analysis-Basics.ipynb  .
 github add . && github commit -m "sunnychan" && github push
